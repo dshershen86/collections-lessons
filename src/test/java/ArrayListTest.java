c0742a6a4f41f8095fbef0956459dc39787a47b3
Dmytro Shershen <dshershen86@gmail.com>
2025-05-03 20:58:00 +0300

    ArrayList refactoring. Added implementation of Iterable. Update tests.

--- src/test/java/ArrayListTest.java
+++ src/test/java/ArrayListTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ArrayListTest{
+class ArrayListTest {
+
     public ArrayList<String> list;
+
     @BeforeEach
     void setUp() {
         list = new ArrayList<>(){
@@ -16,30 +18,30 @@ public class ArrayListTest{
                 add("D");
                 add("A");
                 add("E");
+                add(null);
 
             }
         };
-
     }
     @DisplayName("Test add without insert place and get")
     @Test
     void add() {
-        list.add("M");
-        assertEquals( "M",list.get(6));
+        list.add( "M");
+        assertEquals( "M",list.get(7));
         list.add("nn");
-        assertEquals("nn",list.get(7));
+        assertEquals("nn",list.get(8));
         list.add( "OP");
-        assertEquals( "OP",list.get(8));
+        assertEquals("OP",list.get(9));
     }
     @DisplayName("Test add with insert place and get")
     @Test
     void testAdd() {
         list.add("NO",2);
         assertEquals("NO",list.get(2));
-        assertEquals(7,list.size());
+        assertEquals(8,list.size());
         list.add("YES",6);
         assertEquals("YES",list.get(6));
-        assertEquals(8,list.size());
+        assertEquals(9,list.size());
     }
 
     @Test
@@ -47,14 +49,15 @@ public class ArrayListTest{
         Object b=list.remove(1);
         assertEquals("B",b);
         assertEquals("C",list.get(1));
-        assertEquals(5,list.size());
+        assertEquals(6,list.size());
     }
 
     @Test
     void get() {
         assertEquals("A",list.get(0));
-        assertEquals("B",list.get(1));
         assertEquals("C",list.get(2));
+        assertEquals("E",list.get(5));
+        assertNull(list.get(6));
     }
 
     @Test
@@ -62,23 +65,27 @@ public class ArrayListTest{
         Object result = list.set("NO",1);
         assertEquals("B",result);
         assertEquals("NO",list.get(1));
-        assertEquals(6,list.size());
+
         result = list.set("YES",4);
         assertEquals("A",result);
         assertEquals("YES",list.get(4));
-        assertEquals(6,list.size());
+
+        result = list.set(null,2);
+        assertEquals("C",result);
+        assertNull(list.get(2));
+
     }
 
     @Test
     void clear() {
         list.clear();
         assertEquals(0,list.size());
-        assertNull(list.get(0));
+
     }
 
     @Test
     void size() {
-        assertEquals(6,list.size());
+        assertEquals(7,list.size());
     }
 
     @Test
@@ -91,24 +98,23 @@ public class ArrayListTest{
     @Test
     void contains() {
         assertTrue(list.contains("A"));
+        assertTrue(list.contains(null));
         assertFalse(list.contains("F"));
     }
 
     @Test
     void indexOf() {
         assertEquals(0,list.indexOf("A"));
+
+        assertEquals(6,list.lastIndexOf(null));
     }
 
     @Test
     void lastIndexOf() {
         assertEquals(4,list.lastIndexOf("A"));
+
+        assertEquals(6,list.lastIndexOf(null));
     }
 
-    @Test
-    void hasNextAndNext() {
-        assertTrue(list.hasNext());
-        assertEquals("A",list.next());
-        assertEquals("B",list.next());
-    }
 
 }
